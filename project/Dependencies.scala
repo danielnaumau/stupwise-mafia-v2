3f093c3c5ec1e20961b81243d93e4e221cdbeef8
danielnaumau <sadfreddy@gmail.com>
2022-02-03 18:41:27 +0100

    add redis

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,8 @@ object Dependencies {
   object V {
     val http4s = "0.23.7"
     val circe  = "0.14.1"
+    val redis4cats = "1.0.0"
+
   }
 
   object Libraries {
@@ -17,5 +19,7 @@ object Dependencies {
 
     val http4sDsl     = http4s("dsl")
     val http4sServer  = http4s("blaze-server")
+
+    val redis4catsEffects = "dev.profunktor" %% "redis4cats-effects" % V.redis4cats
   }
 }
